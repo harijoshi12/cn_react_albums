5f0c7b3fba4075f8120032b10dcffffb023a8014
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2023-10-19 12:53:37 +0530

    header ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Header = () => {
-  return (  <h1 className="text-4xl text-center my-4 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md pb-5">
-  Lorem Epsome Album
-</h1>)
+  return ( 
+  <div className="sticky top-0 z-50 bg-white bg-opacity-70">
+  <h1 className="text-4xl text-center my-4 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md pb-5">
+    Lorem Epsome Album
+  </h1>
+</div>
+)
 };
 
 export default Header;

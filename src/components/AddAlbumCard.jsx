40ce4a7d30cd2ac4ee8daa1b11be7f7a4de1b736
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2023-10-19 12:48:42 +0530

    skelton card ready

--- src/components/AddAlbumCard.jsx
+++ src/components/AddAlbumCard.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 
 const AddAlbumCard = ({handleAddOrUpdateAlbum}) => {
   return (
-    <div className="bg-gray-300 p-4 rounded flex items-center justify-center">
-      <button
-        onClick={() => handleAddOrUpdateAlbum()}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Add New Album
-      </button>
-      <div className="animate-pulse flex space-x-4">
-        <div className="rounded-full bg-blue-400 h-12 w-12"></div>
-        <div className="flex-1 space-y-4 py-1">
-          <div className="h-4 bg-blue-400 rounded w-3/4"></div>
-          <div className="space-y-2">
-            <div className="h-4 bg-blue-400 rounded"></div>
-            <div className="h-4 bg-blue-400 rounded w-5/6"></div>
-          </div>
-        </div>
+    <div
+    onClick={() => handleAddOrUpdateAlbum()}
+    className="cursor-pointer bg-gray-300 p-4 rounded shadow-lg hover:bg-gray-400 transition duration-300 ease-in-out flex flex-col justify-between h-full relative"
+  >
+     <div className="absolute inset-0 flex items-center justify-center">
+        <span className="text-3xl font-bold text-gray-700 opacity-50 text-center z-10">
+          Add New Album
+        </span>
       </div>
+    <div>
+      <h2 className="animate-pulse bg-gray-400 h-6 mb-2 w-1/4"></h2>
+      <p className="animate-pulse bg-gray-400 h-4 mb-2 w-3/4"></p>
+      <p className="animate-pulse bg-gray-400 h-4 mb-2 w-3/4"></p>
     </div>
+    <div className="flex justify-between">
+      <button className="animate-pulse bg-gray-400 h-6 w-16"></button>
+      <button className="animate-pulse bg-gray-400 h-6 w-16"></button>
+    </div>
+  </div>
   )
 }
 

1895964525046bcbb5739796d63f6b551507ee4d
harijoshi12 <mr.hariprasadjoshi@gmail.com>
2023-11-07 16:01:44 +0530

    comments added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import AddAlbumCard from './components/AddAlbumCard';
 import AlbumModal from './components/AlbumModal';
 import { Spinner } from './components/Spinner';
 function App() {
+  // State to store albums, initially tries to load from local storage
   const [albums, setAlbums] = useState(() => {
     // Try to get albums from local storage on initial render
     const localData = localStorage.getItem('albums');
     return localData ? JSON.parse(localData) : [];
   });
 
+  // RTK Query hooks for fetching and mutating data
   const {
     data: fetchedAlbums,
     isError,
@@ -21,10 +23,12 @@ function App() {
   } = useGetAlbumsQuery(undefined, {
     skip: albums.length > 0, // Skip fetching if we already have albums
   });
+  // Mutation hooks for adding, updating, and deleting albums
   const [addAlbum, { isError: isAddError, error: addError, isLoading: isAdding, isSuccess: isAddSuccess }] = useAddAlbumMutation();
   const [updateAlbum, { isError: isUpdateError, error: updateError, isLoading: isUpdating }] = useUpdateAlbumMutation();
   const [deleteAlbum, { isError: isDeleteError, error: deleteError, isLoading: isDeleting }] = useDeleteAlbumMutation();
 
+  // State for modal visibility and the current album being edited
   const [showModal, setShowModal] = useState(false);
   const [currentAlbum, setCurrentAlbum] = useState(null);
 
@@ -36,6 +40,7 @@ function App() {
     }
   }, [fetchedAlbums, isLoading]);
 
+  // Handlers for modal actions
   const handleOpenModal = (album = null) => {
     setCurrentAlbum(album);
     setShowModal(true);
@@ -45,6 +50,7 @@ function App() {
     setShowModal(false);
   };
 
+  // Handler for adding or updating an album
   const handleAddOrUpdateAlbum = async (album) => {
     if (album.id) {
       // Update album
@@ -62,6 +68,7 @@ function App() {
     handleCloseModal();
   };
 
+  // Handler for deleting an album
   const handleDeleteAlbum = async (id) => {
     await deleteAlbum(id).unwrap();
     // Update local storage by removing the deleted album
@@ -70,7 +77,7 @@ function App() {
     localStorage.setItem('albums', JSON.stringify(updatedAlbums));
   };
 
-  // Display loading or error messages
+  // Conditional rendering based on loading and error states
   let content
   if (isLoading || isAdding || isUpdating || isDeleting) {
     content = <Spinner />;
@@ -78,16 +85,18 @@ function App() {
     const errorMessage = error || addError || updateError || deleteError;
     content = <h1>Error: {errorMessage?.data?.message || 'An error occurred'}</h1>;
   } else {
-    content =(
+    // Render album cards and add album card
+    content = (
       <>
         <AddAlbumCard handleOpenModal={handleOpenModal} />
         {albums.map((album, idx) => (
           <AlbumCard key={album.id} title={`Album ${idx + 1}`} album={album} handleOpenModal={handleOpenModal} handleDeleteAlbum={handleDeleteAlbum} />
         ))}
       </>
-      )
+    )
   }
 
+  // Main component render
   return (
     <div className="container mx-auto">
       <Header />
